c821105a9ec159c0cf1d696319f11c69b28ea31c
themyav <Myavochka119@gmail.com>
2024-02-24 19:31:44 +0300

    Connection to local ClickHouse database + simple queries

--- src/ru/itmo/potatocoder228/androidlab2024/Main.kt
+++ src/ru/itmo/potatocoder228/androidlab2024/Main.kt
@@ -1,3 +1,11 @@
+
 fun main() {
     println("Hello, world!")
+    val clickHouseManager = ClickHouseManager()
+    clickHouseManager.initConnection()
+    clickHouseManager.createExampleTable()
+    clickHouseManager.createExampleQuery()
+    clickHouseManager.closeConnection()
+
+
 }
